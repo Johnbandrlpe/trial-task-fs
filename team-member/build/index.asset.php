4a5ccf94f930afddec6882890c68d1690f450340
John <johnmichaelbandarlipe14@gmail.com>
2023-11-07 09:28:09 +0800

    added custom block setup

---
+++ team-member/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ce680e5a5072b8ad81f6');
---
+++ team-member/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3f67271986c3d141e8f6');
